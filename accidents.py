1b013291d71a67f7d13d06c33160b5ce3c855e54
daniel <danielnobre09@gmail.com>
2024-06-12 23:57:38 -0300

    fix

--- accidents.py
+++ accidents.py
@@ -1,14 +1,21 @@
-# make sure to install these packages before running:
-# pip install pandas
-# pip install sodapy
-
-import pandas as pd
+from flask import Flask, jsonify
 from sodapy import Socrata
+import pandas as pd
 
+app = Flask(__name__)
+
+# Inicialize o cliente Socrata
 client = Socrata("data.nashville.gov", None)
 
-results = client.get("6v6w-hpcw", limit=20)
+@app.route('/accidents', methods=['GET'])
+def get_accidents():
+    # Obtenha resultados do dataset
+    results = client.get("6v6w-hpcw", limit=20)
+    # Converta os resultados em um DataFrame pandas
+    results_df = pd.DataFrame.from_records(results)
+    # Converta o DataFrame em JSON
+    results_json = results_df.to_json(orient="records")
+    return results_json
 
-# Convert to pandas DataFrame
-results_df = pd.DataFrame.from_records(results)
-print(results_df)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
