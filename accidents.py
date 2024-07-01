65eee11b9ea423ae3da872f4a3e164da1803d372
daniel <danielnobre09@gmail.com>
2024-06-30 21:38:53 -0300

    teste

--- accidents.py
+++ accidents.py
@@ -1,40 +1,46 @@
-from flask import Flask, jsonify, request
-from sodapy import Socrata
+from flask import Flask, jsonify, request, send_file
 import pandas as pd
 from datetime import datetime, timedelta
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import io
 
 app = Flask(__name__)
 
-# Inicialize o cliente Socrata
-client = Socrata("data.nashville.gov", None)
+# Conecte-se ao MongoDB
+mongo_uri = "mongodb+srv://admin:admin@socrata.y2z5nfp.mongodb.net/Socrata"
+mongo_client = MongoClient(mongo_uri, server_api=ServerApi('1'))
 
-def get_last_24_hours_data(dataset_id, client):
-    # Defina o timestamp para 24 horas atrás
+try:
+    mongo_client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# Função para obter dados das últimas 24 horas do MongoDB
+def get_last_24_hours_data_from_mongo(collection_name):
+    db = mongo_client.Socrata
+    collection = db[collection_name]
     last_24_hours = datetime.now() - timedelta(hours=24)
-    last_24_hours_str = last_24_hours.strftime("%Y-%m-%dT%H:%M:%S.%f")
-
-    # Query para obter dados das últimas 24 horas
-    query = f"date_time >= '{last_24_hours_str}'"
-    results = client.get(dataset_id, where=query, limit=20)
-
-    return results
+    results = collection.find({"date_time": {"$gte": last_24_hours}})
+    return list(results)
 
 class DataAdapter:
     @staticmethod
-    def format_data(data):
-        # Converte os dados em um DataFrame pandas
-        results_df = pd.DataFrame.from_records(data)
-        # Converte o DataFrame em JSON
-        results_json = results_df.to_json(orient="records")
-        return results_json
+    def format_data_to_csv(data):
+        df = pd.DataFrame(data)
+        output = io.StringIO()
+        df.to_csv(output, index=False)
+        output.seek(0)
+        return output
 
 @app.route('/accidents', methods=['GET'])
 def get_accidents():
-    # Obtenha resultados do dataset das últimas 24 horas
-    results = get_last_24_hours_data("6v6w-hpcw", client)
-    # Formate os dados usando o adapter
-    formatted_data = DataAdapter.format_data(results)
-    return formatted_data
+    # Obtenha resultados do MongoDB das últimas 24 horas
+    results = get_last_24_hours_data_from_mongo("accidents_prf_collection")
+    # Formate os dados em CSV usando o adapter
+    csv_data = DataAdapter.format_data_to_csv(results)
+    return send_file(io.BytesIO(csv_data.getvalue().encode()), mimetype='text/csv', as_attachment=True, attachment_filename='accidents.csv')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
